a31aecac20cb9d71e5084ee690dce8e3ea3d034b
Sekigahara <androaprila12@gmail.com>
2020-12-01 22:35:27 +0700

    first commit

---
+++ app/src/main/java/com/example/foodprint/model/restaurant/Restaurant.java
@@ -0,0 +1,52 @@
+package com.example.foodprint.model.restaurant;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Restaurant {
+
+    @SerializedName("html_attributions")
+    @Expose
+    private List<Object> htmlAttributions = null;
+    @SerializedName("next_page_token")
+    @Expose
+    private String nextPageToken;
+    @SerializedName("results")
+    @Expose
+    private List<DataRestaurant> results = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public List<Object> getHtmlAttributions() {
+        return htmlAttributions;
+    }
+
+    public void setHtmlAttributions(List<Object> htmlAttributions) {
+        this.htmlAttributions = htmlAttributions;
+    }
+
+    public String getNextPageToken() {
+        return nextPageToken;
+    }
+
+    public void setNextPageToken(String nextPageToken) {
+        this.nextPageToken = nextPageToken;
+    }
+
+    public List<DataRestaurant> getResults() {
+        return results;
+    }
+
+    public void setResults(List<DataRestaurant> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
